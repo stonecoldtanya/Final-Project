468ca7f77c7757870ab74fd445e6f70982671184
Plato <tanqdb.19@abv.bg>
2023-01-24 00:51:51 +0200

    first commit

---
+++ checkers/src/test/java/bg/reachup/edu/checkers/CheckersApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.reachup.edu.checkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
